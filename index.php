9b6925ba74d32e38550ec1f67c898a6a2d37cc19
Ale Mohamad <hello@alemohamad.com>
2017-01-30 00:51:56 -0300

    Episodio 2

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 
 require_once 'vendor/autoload.php';
 
+WingCommander::init();
+
+Flight::set('flight.base_url', 'http://localhost:1234/');
+
 Flight::route('/', function() {
-    echo 'Tenemos nuestra página web!';
+    Flight::render('home', array(), 'yield');
+    Flight::render('menu', array('base_url' => Flight::get('flight.base_url'), 'section_home' => true), 'menu');
+    Flight::render('layout', array());
 });
 
-Flight::route('/hola-mundo', function() {
-    echo 'Hola mundo!';
+Flight::route('/hola/@name', function($name) {
+    Flight::render('hello', array('planet' => $name), 'yield');
+    Flight::render('menu', array('base_url' => Flight::get('flight.base_url'), 'section_hello' => true), 'menu');
+    Flight::render('layout', array());
+});
+
+Flight::route('/contacto', function() {
+    Flight::render('contact', array(), 'yield');
+    Flight::render('menu', array('base_url' => Flight::get('flight.base_url'), 'section_contact' => true), 'menu');
+    Flight::render('layout', array());
 });
 
 Flight::map('notFound', function() {
